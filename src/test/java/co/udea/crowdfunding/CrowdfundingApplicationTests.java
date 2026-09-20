ff3f100bfe0aa884c90f8e15c63c2dab3209b1de
Juan Esteban Yepes Martinez <juan.yepes11@udea.edu.co>
2026-09-20 15:47:17 -0500

    Initial Spring Boot project

---
+++ src/test/java/co/udea/crowdfunding/CrowdfundingApplicationTests.java
@@ -0,0 +1,13 @@
+package co.udea.crowdfunding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrowdfundingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
